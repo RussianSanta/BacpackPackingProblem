a0932eef4c13d3b8d4f3b6e54eb5e86c070700dd
Maks <zheltov2001@gmail.com>
2022-12-14 20:15:24 +0300

    added comments

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -38,19 +38,20 @@ public class Solver {
                         bp[0][j] = new Cell(cellItems, currItem.getCost());
                     } else bp[0][j] = new Cell(new ArrayList<>(), 0);
                 } else {
-                    if (currItem.getWeight() > currWeight) //если очередной предмет не влезает в рюкзак,
-                        bp[i][j] = bp[i - 1][j];    //записываем предыдущий максимум
+//                  Если предмет не влез, то берем предыдущее значение
+                    if (currItem.getWeight() > currWeight)
+                        bp[i][j] = bp[i - 1][j];
+//                  Если влез, то считаем итоговую цену, складывая цену предмета и максимальную цену для оставшегося веса
                     else {
-                        /*рассчитаем цену очередного предмета + максимальную цену для (максимально возможный для рюкзака вес − вес предмета)*/
                         int newPrice;
                         if (currWeight - currItem.getWeight() > 0)
                             newPrice = currItem.getCost() + bp[i - 1][currWeight - currItem.getWeight() - 1].getPrice();
                         else newPrice = currItem.getCost();
-
-                        if (bp[i - 1][j].getPrice() >= newPrice) //если предыдущий максимум больше
+//                      Если предыдущий максимум был больше, то его и запишем
+                        if (bp[i - 1][j].getPrice() >= newPrice)
                             bp[i][j] = bp[i - 1][j]; //запишем его
+//                      Если новая цена оказалась больше, то внесем информацию в таблицу
                         else {
-                            /*иначе фиксируем новый максимум: текущий предмет + стоимость свободного пространства*/
                             ArrayList<Item> cellItems = new ArrayList<>();
                             cellItems.add(currItem);
                             if (currWeight - currItem.getWeight() > 0)
@@ -61,14 +62,14 @@ public class Solver {
                 }
             }
         }
-
+//      Вывод таблицы, можно закомментировать
         for (int i = 0; i < items.size(); i++) {
             for (int j = 0; j < weight; j++) {
                 System.out.print(bp[i][j].getPrice() + " ");
             }
             System.out.print("\n");
         }
-
+//      Поиск максимального значения цены в последнем столбце
         int max = -1;
         ArrayList<Item> resultItems = new ArrayList<>();
         for (int i = 0; i < items.size(); i++) {
